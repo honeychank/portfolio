3604741f1152e8df13ed2b04d43fd396a21a531b
honeychank <63727313+honeychank@users.noreply.github.com>
2020-05-03 15:04:09 +0900

    Create jquery.js

---
+++ folder/jquery.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  $('.about-img').waypoint(function(direction){
+    var activePoint = $(this.element);
+    if (direction === 'down') { //scroll down
+        activePoint.addClass('slideIn-left');
+    }
+    else{ //scroll up
+        activePoint.removeClass('slideIn-left');
+    }
+  },{offset : '80%'});
+
+  $('.about-text').waypoint(function(direction){
+    var activePoint = $(this.element);
+    if (direction === 'down') { //scroll down
+        activePoint.addClass('slideIn-right');
+    }
+    else{ //scroll up
+        activePoint.removeClass('slideIn-right');
+    }
+  },{offset : '80%'});
+
+});
